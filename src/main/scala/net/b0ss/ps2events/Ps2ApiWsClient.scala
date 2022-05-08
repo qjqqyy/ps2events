edc3b9a1446315fcf285be40af50069f90a619f5
Qi Ji <qjqqyy@gmail.com>
2022-05-09 00:52:11 +0800

    reconnect to websocket every 15min

--- src/main/scala/net/b0ss/ps2events/Ps2ApiWsClient.scala
+++ src/main/scala/net/b0ss/ps2events/Ps2ApiWsClient.scala
@@ -5,11 +5,14 @@ import sttp.client3._
 import sttp.client3.httpclient.HttpClientFutureBackend
 import sttp.ws.WebSocket
 
+import java.util.{ Timer, TimerTask }
 import scala.concurrent._
 import scala.concurrent.duration.DurationInt
 
 class Ps2ApiWsClient(serviceId: String, consume: String => Unit)(implicit ec: ExecutionContext) {
   private val backend = HttpClientFutureBackend()
+  private val timer = new Timer("ps2-ws-client-quit-timer")
+  private var wsOption: Option[WebSocket[Future]] = None
 
   private def consumeAllMessages(ws: WebSocket[Future]): Future[Unit] =
     ws.receiveText()
@@ -17,10 +20,15 @@ class Ps2ApiWsClient(serviceId: String, consume: String => Unit)(implicit ec: Ex
       .flatMap(_ => consumeAllMessages(ws))
 
   private def handleWebsocket(ws: WebSocket[Future]): Future[Unit] = {
-    for {
-      _ <- ws.sendText(SUBSCRIBE_ALL)
-      _ <- consumeAllMessages(ws)
-    } yield ()
+    wsOption = Some(ws)
+    timer.schedule(
+      new TimerTask {
+        def run(): Unit = ws.close()
+      },
+      15 * 60 * 1000,
+    )
+    ws.sendText(SUBSCRIBE_ALL)
+      .flatMap(_ => consumeAllMessages(ws))
   }
 
   def start(): Future[Response[Either[String, Unit]]] = basicRequest
@@ -28,10 +36,13 @@ class Ps2ApiWsClient(serviceId: String, consume: String => Unit)(implicit ec: Ex
     .response(asWebSocket(handleWebsocket))
     .send(backend)
 
-  def close(): Unit = try {
-    Await.result(backend.close(), 5.seconds)
-  } catch {
-    case _: java.util.concurrent.TimeoutException =>
+  def close(): Unit = {
+    timer.cancel()
+    try {
+      Await.result(Future.sequence(wsOption.map(_.close()).toVector :+ backend.close()), 5.seconds)
+    } catch {
+      case _: java.util.concurrent.TimeoutException =>
+    }
   }
 
 }
